df16cac5a88d35a59e8ffa2cb219d0d3c24f1918
David Broadfoot <witenite@witenite.ca>
2024-04-21 12:35:23 -0600

    Add more discriptions and a second command, in comments.

--- Info-search/list-apt-sources.sh
+++ Info-search/list-apt-sources.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 grep -rihv -e '^#' /etc/apt/sources.list* | grep -v ^[[:space:]]*$
 
+
 # Recursive, Ignore Filename, Exclube lines beinning with '#' or a space. 
 # Usees grep twice with a redirect pipe.
+
+# Another way to do have the same result.
 #
+# grep -rhvE '^$|#' /etc/apt/sources.list*
