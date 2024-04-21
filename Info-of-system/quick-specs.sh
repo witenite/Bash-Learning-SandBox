9ad25cbb4a393c6d5bb22c9657e635c446ced960
David Broadfoot <witenite@witenite.ca>
2024-04-21 13:31:46 -0600

    Update and expand quick-info.sh

--- Info-of-system/quick-specs.sh
+++ Info-of-system/quick-specs.sh
@@ -1,3 +1,21 @@
 #/bin/bash
+echo ""
+echo "Welcome $(whoami)"
+echo ""
+echo "You are using $(hostname)"
+echo ""
+echo "Your current memory useage is as follows."
+echo ""
+free -ht
+echo ""
+echo "Your current disk utilization is as follows."
+echo ""
+df -h
+echo ""
+echo ""
+echo "Your current home directory disk useage."
+echo ""
+du -hcs /home/$(whoami)
+echo ""
+echo ""
 
-uname -a
